130bde94f0fdabc96671141944bb54279f909714
Ravi <ravi7284007@gmail.com>
2021-09-29 06:43:08 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {Nav, Navbar, Container} from 'react-bootstrap';
+import {Link, Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import User from './User';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         
+    <div className="App">
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">React PWA</Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link ><Link to="/about" >About</Link></Nav.Link>
+            <Nav.Link><Link to="/user">User</Link></Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route path="/about" component={About} ></Route>
+          <Route path="/user" component={User} ></Route>
+          <Route path="/" component={Home} ></Route>
+        </Switch>
+      </Router>
+    </div>
     </div>
   );
 }
+
+
 
 export default App;
